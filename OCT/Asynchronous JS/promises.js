19fc18b22198584a6db248c13b0cbea67438a1ad
Apil Adhikari <apiladhikari207@gmail.com>
2023-10-26 14:16:00 +0545

    Add Async/Await t in the code

--- OCT/Asynchronous JS/promises.js
+++ OCT/Asynchronous JS/promises.js
@@ -34,16 +34,25 @@ const createPost = (post) => {
 //   .then(getPosts)
 //   .catch((err) => console.log(err));
 
-// Promise.all()
-const promise1 = Promise.resolve("Hello world");
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(resolve, 2000, "Good Bye")
-);
-const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
-  (res) => res.json()
-);
+// Async / Await
+const init = async () => {
+  await createPost({ title: "Post three", body: "This is post three." });
 
-Promise.all([promise1, promise2, promise3, promise4]).then((vlaues) =>
-  console.log(vlaues)
-);
+  getPosts();
+};
+
+init();
+
+// Promise.all()
+// const promise1 = Promise.resolve("Hello world");
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//   setTimeout(resolve, 2000, "Good Bye")
+// );
+// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
+//   (res) => res.json()
+// );
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((vlaues) =>
+//   console.log(vlaues)
+// );
