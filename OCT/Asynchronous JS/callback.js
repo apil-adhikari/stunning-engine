6e21c8236c7ff782a02d6e5333f459f9660e61f6
Apil Adhikari <apiladhikari207@gmail.com>
2023-10-26 12:55:01 +0545

    Add synchronous code block which broke the regular flow

--- OCT/Asynchronous JS/callback.js
+++ OCT/Asynchronous JS/callback.js
@@ -13,4 +13,17 @@ const getPosts = () => {
   }, 1000);
 };
 
+// post is pushed after 2 seconds
+const createPost = (post) => {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+};
+
 getPosts();
+
+// calling createPost()
+createPost({ title: "Post three", body: "This is post three." });
+
+// Only 2 posts are shown in list as createPost() takes 2 seconds to run but getPosts() finishes displaying each post in 1 second.
+// [To avoid this from happening, we use asynchronous way.]
